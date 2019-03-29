7f03066d1867eb4c81674cdd6298d9c12ab58d77
Andrew Skiles <networking@andyskiles.com>
2019-03-29 11:51:23 -0400

    Add react build

---
+++ implementation/web-react/build/precache-manifest.fb226fd3d24a4d909e0aeca07702f8d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d967bdc12e168a8a89c3",
+    "url": "/static/js/main.180d5976.chunk.js"
+  },
+  {
+    "revision": "32bfe6000434f04292fa",
+    "url": "/static/js/2.b3b50664.chunk.js"
+  },
+  {
+    "revision": "d967bdc12e168a8a89c3",
+    "url": "/static/css/main.3a9c7c49.chunk.css"
+  },
+  {
+    "revision": "7527866c94665d089eb3761d7bac504c",
+    "url": "/index.html"
+  }
+];
